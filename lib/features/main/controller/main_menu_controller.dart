378b5c0f8ab68efd2632642ccffdf6888d2b921a
Muhammad Yaasiin Hidayatulloh <myaasiinh@gmail.com>
2024-10-15 09:23:57 +0700

    fix struktur

--- lib/features/main/controller/main_menu_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MainMenuController with ChangeNotifier {
-  String location = "Seoul";
-  void updateLocation(String newLocation) {
-    location = newLocation;
-    notifyListeners();
-  }
-}
